b9f2ebf6f11799a9b56c1c931ef817c4df49a34b
Aaron Sky <aaronsky@skyaaron.com>
2020-01-20 23:03:46 -0500

    Re-added main.go

---
+++ cmd/codeowners-coverage/main.go
@@ -0,0 +1,10 @@
+// Package main is the command line interface to package coverage
+package main
+
+import (
+	"github.com/aaronsky/codeowners-coverage/internal/cli"
+)
+
+func main() {
+	cli.CLI()
+}
